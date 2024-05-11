c04164b3a0e22c3656d5f5a3e415604c09c53e41
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-05-11 06:40:28 -0300

    feat: new logo

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import { Navbar } from "./navbar";
 
 const Header = () => {
   //<Image src="/logo.png" alt="logo" width={120} height={30} priority />
   return (
     <div className="flex justify-between px-5 pt-6">
-      <h1 className="text-2xl font-extrabold text-red-600">ÉFooda</h1>
+      <Image src="/logo.png" alt="logo" width={50} height={50} priority />
       <Navbar />
     </div>
   );
